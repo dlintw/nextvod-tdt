12e6e35303d10661d11117271c3a3bcd2cae7d5c
pinky1981 <al_borland33@yahoo.de>
2009-12-30 18:20:01 +0100

    Webif: Y_Baselib.js + Y_VLC.js, removed Y_VLC.js.gz + Y_Baselib.js.gz, TTF: set micron.ttf=neutrino.tff, delete other unused fonts

--- tdt/cvs/cdk/make/release_neutrino.mk
+++ tdt/cvs/cdk/make/release_neutrino.mk
@@ -533,8 +533,14 @@ endif
 	cp -aR $(targetprefix)/usr/local/share/neutrino $(prefix)/release_neutrino/usr/local/share/
 #	TODO: HACK
 	cp -aR $(targetprefix)/$(targetprefix)/usr/local/share/neutrino/* $(prefix)/release_neutrino/usr/local/share/neutrino
-	cp -aR $(targetprefix)/usr/local/share/fonts $(prefix)/release_neutrino/usr/local/share/
-	ln -s /usr/local/share/fonts/micron.ttf $(prefix)/release_neutrino/usr/local/share/fonts/neutrino.ttf
+######## changed by pinky 1981 ######################################################################
+#	cp -aR $(targetprefix)/usr/local/share/fonts $(prefix)/release_neutrino/usr/local/share/
+	mkdir -p $(prefix)/release_neutrino/usr/local/share/fonts
+	cp -aR $(targetprefix)/usr/local/share/fonts/micron.ttf $(prefix)/release_neutrino/usr/local/share/fonts/neutrino.ttf
+	cp $(appsdir)/neutrino/src/nhttpd/web/Y_Baselib.js $(prefix)/release_neutrino/usr/local/share/neutrino/httpd-y/
+	cp $(appsdir)/neutrino/src/nhttpd/web/Y_VLC.js $(prefix)/release_neutrino/usr/local/share/neutrino/httpd-y/
+	rm $(prefix)/release_neutrino/usr/local/share/neutrino/httpd-y/Y_Baselib.js.gz
+	rm $(prefix)/release_neutrino/usr/local/share/neutrino/httpd-y/Y_VLC.js.gz
 
 
 #######################################################################################
