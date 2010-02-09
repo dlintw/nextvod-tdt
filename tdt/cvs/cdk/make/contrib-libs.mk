820d5c2cc858e918b6724c23adb4eec3eee4f562
willi68 <willi68@rocketmail.com>
2010-02-09 07:30:33 +0100

    fixed lirc build.

--- tdt/cvs/cdk/make/contrib-libs.mk
+++ tdt/cvs/cdk/make/contrib-libs.mk
@@ -100,6 +100,7 @@ $(DEPDIR)/lirc.do_prepare: bootstrap @DEPENDS_lirc@
 $(DEPDIR)/lirc.do_compile: $(DEPDIR)/lirc.do_prepare
 	cd @DIR_lirc@ && \
 		$(BUILDENV) \
+		ac_cv_path_LIBUSB_CONFIG= \
 		./configure \
 			--build=$(build) \
 			--host=$(target) \
