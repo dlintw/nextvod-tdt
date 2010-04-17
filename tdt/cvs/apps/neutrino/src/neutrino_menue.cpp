fe69c14993caebb48d5e502c6ba590e40ce7495b
BPanther <bpanther_ts@hotmail.com>
2010-04-17 15:00:01 +0200

    [neutrino] - red icon for internet radio

--- tdt/cvs/apps/neutrino/src/neutrino_menue.cpp
+++ tdt/cvs/apps/neutrino/src/neutrino_menue.cpp
@@ -2845,7 +2845,7 @@ bool CNeutrinoApp::showUserMenu(int button)
 	if (button == SNeutrinoSettings::BUTTON_BLUE)
 	{
 	audioPlayer = new CAudioPlayerGui(true);
-	menu->addItem(new CMenuForwarder(LOCALE_INETRADIO_NAME, true, NULL, audioPlayer, NULL, CRCInput::RC_red));
+	menu->addItem(new CMenuForwarder(LOCALE_INETRADIO_NAME, true, NULL, audioPlayer, NULL, CRCInput::RC_red, NEUTRINO_ICON_BUTTON_RED));
 	}
 
         // go through any postition number
