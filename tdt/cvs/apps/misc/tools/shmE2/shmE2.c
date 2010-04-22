ad27cb0b6febbd3f2c8a1eb44700370bc13bd5f3
nit <o.gruber@nit>
2010-04-22 11:02:50 +0200

    [e2] add tool for ipc shm

---
+++ tdt/cvs/apps/misc/tools/shmE2/shmE2.c
@@ -0,0 +1,73 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include "shmE2.h"
+
+main(int argc, char *argv[])
+{
+	if(argc != 3 || (strcmp(argv[1], "set") != 0 && strcmp(argv[1], "del") != 0 && strcmp(argv[1], "get") != 0 && strcmp(argv[1], "check") != 0)) {
+		printf("Usage: %s set variable=value\n", argv[0]);
+		printf("Usage: %s del all\n", argv[0]);
+		printf("Usage: %s del variable\n", argv[0]);
+		printf("Usage: %s get all\n", argv[0]);
+		printf("Usage: %s get variable\n", argv[0]);
+		printf("Usage: %s check variable\n", argv[0]);
+		exit(0);
+	}
+
+	char *shm = NULL;
+
+	shm = findshm();
+	if(shm == NULL) {
+		perror("findshm");
+		exit(1);
+	}
+
+	if(strcmp(argv[1], "get") == 0 && strcmp(argv[2], "all") != 0) {
+		char *shmbuf = NULL;
+		shmbuf = malloc(256);
+		strcat(argv[2], "=");
+		getshmentry(shm, argv[2], shmbuf, 256);
+		printf("%s\n", shmbuf);
+		free(shmbuf);
+		exit(0);
+	}
+
+        if(strcmp(argv[1], "get") == 0 && strcmp(argv[2], "all") == 0) {
+		char *shmbuf = NULL;
+		shmbuf = malloc(4096);
+                getshmentryall(shm, shmbuf, 4096);
+                printf("%s\n", shmbuf);
+		free(shmbuf);
+                exit(0);
+        }
+
+        if(strcmp(argv[1], "set") == 0) {
+                if(setshmentry(shm, argv[2]) != 0)
+			printf("error: value not set\n");
+                exit(0);
+        }
+
+        if(strcmp(argv[1], "del") == 0 && strcmp(argv[2], "all") != 0) {
+		strcat(argv[2], "=");
+                delshmentry(shm, argv[2]);
+                exit(0);
+        }
+
+        if(strcmp(argv[1], "del") == 0 && strcmp(argv[2], "all") == 0) {
+                *shm = '\0';
+                exit(0);
+        }
+
+        if(strcmp(argv[1], "check") == 0) {
+		strcat(argv[2], "=");
+		if(checkshmentry(shm, argv[2]) == 0)
+			printf("not exist\n");
+		else
+			printf("exist\n");
+                exit(0);
+	}
+	
+	exit(0);
+}
---
+++ tdt/cvs/apps/misc/tools/shmE2/shmE2.h
@@ -0,0 +1,242 @@
+#ifndef SHME2
+#define SHME2
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHMSZ 27
+#define KEY 1234
+
+static char* createshm()
+{
+        int shmid;
+        key_t key = KEY;
+	char *shm = NULL;
+
+        if((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
+                return NULL;
+        }
+
+        shm = (char *) shmat(shmid, NULL, 0);
+        if(shm == (char *) -1)
+		return NULL;
+
+        *shm = '\0';
+
+	return shm;
+}
+
+static char* findshm()
+{
+        int shmid;
+        key_t key = KEY;
+	char *shm = NULL;
+
+        if((shmid = shmget(key, SHMSZ, 0666)) < 0) {
+                return NULL;
+        }
+
+        shm = (char *) shmat(shmid, NULL, 0);
+        if(shm == (char *) -1)
+                return NULL;
+
+        return shm;
+}
+
+static int delshmentry(char *shm, char *key)
+{
+	if(shm == NULL) return -1;
+        char *vbuf = NULL, *shmbuf = NULL;
+	char *v, *s = shm;
+        int matchkey = 0;
+	int ret = 0;
+
+	vbuf = (char *) malloc(256);
+	if(vbuf == NULL)
+		return -1;
+	v = vbuf;
+
+	//FIXME: here we can have a segfault
+	shmbuf = (char *) malloc(4096);
+	if(shmbuf == NULL)
+		return -1;
+
+        while(*s != '\0')
+        {
+                *v++ = *s++;
+                *v = '\0';
+                if(strcmp(key, vbuf) == 0)
+		{
+                        matchkey = 1;
+			ret = 1;
+		}
+                if(matchkey == 0 && *s == ';')
+                {
+			*v++ = ';';
+			*v = '\0';
+			strcat(shmbuf, vbuf);
+                        v = vbuf;
+                        s++;
+                }
+                if(matchkey == 1 && *s == ';')
+		{
+			matchkey = 0;
+			v = vbuf;
+			s++;
+		}
+        }
+        if(matchkey == 0)
+        {
+		*v++ = ';';
+		*v = '\0';
+		strcat(shmbuf, vbuf);
+                v = vbuf;
+        }
+	shmbuf[strlen(shmbuf)-1] = '\0';
+        *shm = '\0';
+	strcpy(shm, shmbuf);
+
+	free(vbuf);
+	free(shmbuf);
+
+        return ret;
+}
+
+static int setshmentry(char *shm, char *entry)
+{
+	if(shm == NULL) return -1;
+	char *buf = NULL;
+	char *c;
+	int pos = 0;
+
+	if(strlen(entry) > 255)
+		return -1;
+
+       	buf = (char *) malloc(256);
+        if(buf == NULL)
+                return -1;
+
+	c = strchr(entry, '=');
+	if(c != NULL)
+		pos = (c - entry) + 1;
+	else
+	{
+		free(buf);
+		return -1;
+	}
+
+	strncpy(buf, entry, pos);
+
+        if(delshmentry(shm, buf) < 0)
+	{
+		free(buf);
+		return -1;
+	}
+
+	if(*shm != '\0')
+		strcat(shm, ";");
+        strcat(shm, entry);
+
+	free(buf);
+	return 1;
+}
+
+static int getshmentry(char *shm, char *key, char *buf, int buflen)
+{
+	if(shm == NULL) return -1;
+        char *v, *s = shm, *shmbuf;
+        int matchkey = 0;
+	int ret = 0;
+
+       	shmbuf = (char *) malloc(256);
+        if(shmbuf == NULL)
+                return -1;
+
+	v = shmbuf;
+
+        while(*s != '\0')
+        {
+                *v++ = *s++;
+                *v = '\0';
+                if(strcmp(key, shmbuf) == 0)
+                {
+                        v = shmbuf;
+                        matchkey = 1;
+			ret = 1;
+                }
+                if(matchkey == 0 && *s == ';')
+                {
+                        v = shmbuf;
+                        s++;
+                }
+                if(matchkey == 1 && *s == ';')
+                        break;
+        }
+        if(matchkey == 0)
+                v = shmbuf;
+        *v = '\0';
+
+	strncpy(buf, shmbuf, buflen-1);
+	free(shmbuf);
+
+        return ret;
+}
+
+static int checkshmentry(char *shm, char *key)
+{
+	if(shm == NULL) return -1;
+        char *v = NULL, *s = shm, *shmbuf = NULL;
+        int ret = 0;
+
+	shmbuf = (char *) malloc(256);
+	if(shmbuf == NULL)
+		return -1;
+
+	v = shmbuf;
+
+        while(*s != '\0')
+        {
+                *v++ = *s++;
+                *v = '\0';
+                if(strcmp(key, shmbuf) == 0)
+                {
+                        ret = 1;
+			break;
+                }
+                if(*s == ';')
+                {
+                        v = shmbuf;
+                        s++;
+                }
+        }
+        return ret;
+}
+
+static int getshmentryall(char *shm, char *shmbuf, int buflen)
+{
+	if(shm == NULL) return -1;
+        char *v = shmbuf, *s = shm;
+	int count = 0;
+
+        while(*s != '\0')
+        {
+                *v++ = *s++;
+                *v = '\0';
+		count++;
+                if(*s == ';')
+                {
+			if(count >= buflen-1)
+				break;
+                        s++;
+			*v++ = '\n';
+			count++;
+                }
+		if(count >= buflen-1)
+			break;
+        }
+        *v = '\0';
+
+	return 0;
+}
+
+#endif
