bf252ef6da35aa00679464e281efe7610315d3b8
konfetti <fossiphant@gmx.net>
2010-04-24 13:19:09 +0200

    [stm23] use renamed patches also for fortis and octagon

--- tdt/cvs/cdk/make/linux-kernel.mk
+++ tdt/cvs/cdk/make/linux-kernel.mk
@@ -173,14 +173,14 @@ UFS922PATCHES_23 = $(COMMONPATCHES_23) \
 		$(if $(P0119),linux-sh4-ufs922_setup_stm23$(PATCH_STR).patch)
 
 FORTISPATCHES_23 = $(COMMONPATCHES_stm23) \
-		fdma_stm23.patch \
-		sound_stm23.diff \
+		$(if $(P0119),linux-sh4-fdma_stm23$(PATCH_STR).patch) \
+		linux-sh4-sound_stm23$(PATCH_STR).patch \
 		fortis_hdbox_setup_stm23.diff \
 		fortis_hdbox_dvb_core_stm23.patch
 
 OCTAGON1008PATCHES_23 = $(COMMONPATCHES_stm23) \
-		fdma_stm23.patch \
-		sound_stm23.diff \
+		$(if $(P0119),linux-sh4-fdma_stm23$(PATCH_STR).patch) \
+		linux-sh4-sound_stm23$(PATCH_STR).patch \
 		fortis_hdbox_setup_stm23.diff \
 		fortis_hdbox_dvb_core_stm23.patch
 
