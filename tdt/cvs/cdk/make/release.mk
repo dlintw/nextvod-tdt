82f0c40f73ca03aff12c596145603af3d009fe79
spark.jun <126186@gmail.com>
2010-08-11 19:54:04 +0800

    add vfd driver for spark

--- tdt/cvs/cdk/make/release.mk
+++ tdt/cvs/cdk/make/release.mk
@@ -127,11 +127,11 @@ release_ufs912:
 
 release_spark:
 	echo "spark" > $(prefix)/release/etc/hostname 
-	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/micom/micom.ko $(prefix)/release/lib/modules/
 	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/stgfb/stmfb/stmcore-display-sti7111.ko $(prefix)/release/lib/modules/
 	cp $(targetprefix)/boot/video_7111.elf $(prefix)/release/boot/video.elf 
 	cp $(targetprefix)/boot/audio_7111.elf $(prefix)/release/boot/audio.elf
 	cp -f $(buildprefix)/root/usr/local/share/enigma2/keymap_spark.xml $(prefix)/release/usr/local/share/enigma2/keymap.xml	
+	cp -f $(buildprefix)/root/release/vfd_spark_stm23_0119.ko $(prefix)/release/lib/modules/vfd.ko
 
 	rm -f $(prefix)/release/lib/firmware/dvb-fe-avl2108.fw
 	rm -f $(prefix)/release/lib/firmware/dvb-fe-stv6306.fw
