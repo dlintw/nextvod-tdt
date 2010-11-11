8d6ac83d1350bd6ecd201f88523f52f2dd501e1d
max10 <max_10@gmx.de>
2010-11-11 19:16:29 +0100

    change back hdbox.c fp_control

--- tdt/cvs/apps/misc/tools/fp_control/HDBOX.c
+++ tdt/cvs/apps/misc/tools/fp_control/HDBOX.c
@@ -198,13 +198,12 @@ static int setTimer(Context_t* context)
    /* failed to read e2 timers so lets take a look if
     * we are running on neutrino
     */
-/*
+
    if (wakeupTime == 3000000000ul)
    {
       wakeupTime = read_neutrino_timers(curTime);
    }
 
-*/
    wakeupTime -= private->wakeupDecrement;
    
    if ((wakeupTime == 0) || (curTime > wakeupTime))
