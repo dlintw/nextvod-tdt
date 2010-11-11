5bdfdff6b6eb06b30ab3f56bcea79eefada2ba8f
max10 <max_10@gmx.de>
2010-11-11 19:02:32 +0100

    [HDBOX] small fix linux-kernel.mk stm23

--- tdt/cvs/apps/misc/tools/fp_control/HDBOX.c
+++ tdt/cvs/apps/misc/tools/fp_control/HDBOX.c
@@ -198,11 +198,13 @@ static int setTimer(Context_t* context)
    /* failed to read e2 timers so lets take a look if
     * we are running on neutrino
     */
+/*
    if (wakeupTime == 3000000000ul)
    {
       wakeupTime = read_neutrino_timers(curTime);
    }
 
+*/
    wakeupTime -= private->wakeupDecrement;
    
    if ((wakeupTime == 0) || (curTime > wakeupTime))
@@ -593,7 +595,6 @@ Model_t HDBOX_model = {
 	getWakeupReason,
 	setLight,
         Exit,
-        NULL,
 	NULL,
 	NULL,
 };
