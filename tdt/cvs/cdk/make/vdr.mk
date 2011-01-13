d7478cc69d14cbed62dfc49c4b77386f4f4916f1
schraatel <cheffe@cheffe-netbook.(none)>
2011-01-13 19:29:59 +0100

    added paramter VDR to vdr.mk to set to 1.7.16 if wanted. Will build 1.7.14 as default

--- tdt/cvs/cdk/make/vdr.mk
+++ tdt/cvs/cdk/make/vdr.mk
@@ -1,6 +1,8 @@
 # vdr
 VERSION_vdr	:= 0.0.2
 DIR_vdr	:= ../apps/vdr
+VDR = 1.7.16
+#VDR = 1.7.14
 
 $(DEPDIR)/vdr.do_compile: bootstrap libstdc++-dev ncurses-dev jpeg libz libpng freetype stgfb-devnew expat fontconfig alsa-lib alsa-lib-dev alsa-utils libboost \
 		alsaplayer alsaplayer-dev
@@ -9,13 +11,13 @@ if ENABLE_VDR1711
 		$(MAKE_OPTS) $(MAKE) vdr plugins prefix=$(targetprefix)
 	touch $@
 else
-	if [ ! -e $(buildprefix)/Archive/vdr-1.7.14_mod.bz2 ]; then \
-		cd $(buildprefix)/Archive/; \ 
-		wget http://kathif.vs120005.hl-users.com/test/vdr-1.7.14_mod.bz2 ; \
+	if [ ! -e $(buildprefix)/Archive/vdr-$(VDR)_mod.bz2 ]; then \
+		cd $(buildprefix)/Archive/; \
+		wget http://kathif.vs120005.hl-users.com/test/vdr-$(VDR)_mod.bz2 ; \
 	fi; 
 	cd $(DIR_vdr) && \
-	tar jxf $(buildprefix)/Archive/vdr-1.7.14_mod.bz2
-	cd $(DIR_vdr)/vdr-1.7.14 && \
+	tar jxf $(buildprefix)/Archive/vdr-$(VDR)_mod.bz2
+	cd $(DIR_vdr)/vdr-$(VDR) && \
 		$(MAKE_OPTS) $(MAKE) vdr plugins prefix=$(targetprefix)
 	touch $@
 endif
@@ -28,17 +30,17 @@ if ENABLE_VDR1711
 	cp $(appsdir)/vdr/vdr-1.7.11/channels.conf $(targetprefix)/var/vdr
 	cp $(appsdir)/vdr/vdr-1.7.11/keymacros.conf $(targetprefix)/var/vdr
 else
-	$(MAKE) -C $(DIR_vdr)/vdr-1.7.14 all install install-plugins prefix=$(targetprefix) DESTDIR=$(targetprefix)
-	cp $(appsdir)/vdr/vdr-1.7.14/PLUGINS/src/remote/misc/remote.conf $(targetprefix)/var/vdr
+	$(MAKE) -C $(DIR_vdr)/vdr-$(VDR) all install install-plugins prefix=$(targetprefix) DESTDIR=$(targetprefix)
+	cp $(appsdir)/vdr/vdr-$(VDR)/PLUGINS/src/remote/misc/remote.conf $(targetprefix)/var/vdr
 	mkdir -p $(targetprefix)/usr/lib/vdr
 	mkdir -p $(targetprefix)/usr/local/bin
-	cp $(appsdir)/vdr/vdr-1.7.14/PLUGINS/lib/libvdr*1.7.14 $(targetprefix)/usr/lib/vdr
-#	cp $(appsdir)/vdr/vdr-1.7.14/sources.conf $(targetprefix)/var/vdr
-#	cp $(appsdir)/vdr/vdr-1.7.14/channels.conf $(targetprefix)/var/vdr
-#	cp $(appsdir)/vdr/vdr-1.7.14/keymacros.conf $(targetprefix)/var/vdr
+	cp $(appsdir)/vdr/vdr-$(VDR)/PLUGINS/lib/libvdr*$(VDR) $(targetprefix)/usr/lib/vdr
+#	cp $(appsdir)/vdr/vdr-$(VDR)/sources.conf $(targetprefix)/var/vdr
+#	cp $(appsdir)/vdr/vdr-$(VDR)/channels.conf $(targetprefix)/var/vdr
+#	cp $(appsdir)/vdr/vdr-$(VDR)/keymacros.conf $(targetprefix)/var/vdr
 endif
 	cp -rd $(appsdir)/vdr/fonts $(targetprefix)/usr/share
-	cp -rd $(appsdir)/vdr/vdr-1.7.14/vdr $(targetprefix)/usr/local/bin
+	cp -rd $(appsdir)/vdr/vdr-$(VDR)/vdr $(targetprefix)/usr/local/bin
 	mkdir -p $(prefix)/release_vdr/usr/include/boost/
 	mkdir -p $(prefix)/release_vdr/usr/lib/vdr/
 	mkdir -p $(prefix)/release_vdr/usr/local/bin/
@@ -49,7 +51,7 @@ endif
 	cp -rd $(targetprefix)/usr/include/boost/shared_container_iterator.hpp $(prefix)/release_vdr/usr/include/boost/
 	cp -rd $(targetprefix)/usr/lib/libfontconfi* $(prefix)/release_vdr/usr/lib/
 	cp -rd $(targetprefix)/usr/lib/vdr/lib* $(prefix)/release_vdr/usr/lib/vdr/
-#	cp -rd $(targetprefix)/usr/lib/vdr/libvdr-vfd.so.1.7.14 $(prefix)/release_vdr/usr/lib/vdr/
+#	cp -rd $(targetprefix)/usr/lib/vdr/libvdr-vfd.so.$(VDR) $(prefix)/release_vdr/usr/lib/vdr/
 	cp -rd $(targetprefix)/usr/local/bin/vdr $(prefix)/release_vdr/usr/local/bin/
 	cp -rd $(targetprefix)/usr/share/locale/* $(prefix)/release_vdr/usr/share/locale
 #	cp -rd $(targetprefix)/var/vdr/remote.conf $(prefix)/release_vdr/var/vdr/
@@ -66,7 +68,7 @@ endif
 	cp $(buildprefix)/root/usr/local/bin/vdrshutdown $(prefix)/release_vdr/usr/local/bin
 	cp -rd $(buildprefix)/root/var/vdr $(prefix)/release_vdr/var
 #	cp $(buildprefix)/root/release/rcS_vdr_ufs910 $(prefix)/release_vdr/etc/init.d/rcS
-	cp $(buildprefix)/root/release/rcS_vdr_1.7.14_ufs910 $(prefix)/release_vdr/etc/init.d/rcS
+	cp $(buildprefix)/root/release/rcS_vdr_$(VDR)_ufs910 $(prefix)/release_vdr/etc/init.d/rcS
 	chmod 755 $(prefix)/release_vdr/etc/init.d/rcS
 
 vdr-clean:
@@ -77,6 +79,6 @@ vdr-distclean:
 if ENABLE_VDR1711
 	$(MAKE) -C $(DIR_vdr)/vdr-1.7.11 clean clean-plugins
 else
-	$(MAKE) -C $(DIR_vdr)/vdr-1.7.14 clean clean-plugins
+	$(MAKE) -C $(DIR_vdr)/vdr-$(VDR) clean clean-plugins
 endif
 	-rm .deps/vdr*
