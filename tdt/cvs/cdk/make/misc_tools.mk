004a1c2a165c0faa2fc678feeb9380f80619514e
BPanther <bpanther_ts@hotmail.com>
2011-04-30 23:59:05 +0200

    [make/ffmpeg] - updated ffmpeg 0.6.1

--- tdt/cvs/cdk/make/misc_tools.mk
+++ tdt/cvs/cdk/make/misc_tools.mk
@@ -10,6 +10,10 @@ $(appsdir)/misc/tools/config.status: bootstrap
 $(DEPDIR)/min-misc-tools $(DEPDIR)/std-misc-tools $(DEPDIR)/max-misc-tools $(DEPDIR)/misc-tools: \
 $(DEPDIR)/%misc-tools: driver libstdc++-dev libdvdnav libdvdcss libpng jpeg ffmpeg $(appsdir)/misc/tools/config.status
 	$(MAKE) -C $(appsdir)/misc/tools all install DESTDIR=$(prefix)/$*cdkroot \
+	$(if $(UFS910),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_UFS910") \
+	$(if $(FLASH_UFS910),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_FLASH_UFS910") \
+	$(if $(FORTIS_HDBOX),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_FORTIS_HDBOX") \
+	$(if $(OCTAGON1008),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_OCTAGON1008") \
 	$(if $(CUBEREVO),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_CUBEREVO") \
 	$(if $(CUBEREVO_MINI),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_CUBEREVO_MINI") \
 	$(if $(CUBEREVO_MINI2),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_CUBEREVO_MINI2") \
@@ -17,6 +21,7 @@ $(DEPDIR)/%misc-tools: driver libstdc++-dev libdvdnav libdvdcss libpng jpeg ffmp
 	$(if $(CUBEREVO_250HD),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_CUBEREVO_250HD") \
 	$(if $(CUBEREVO_2000HD),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_CUBEREVO_2000HD") \
 	$(if $(CUBEREVO_9500HD),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_CUBEREVO_9500HD") \
+	$(if $(ATEVIO7500),CPPFLAGS="$(CPPFLAGS) -DPLATFORM_ATEVIO7500") \
 	$(if $(PLAYER131),CPPFLAGS="$(CPPFLAGS) -DPLAYER131") \
 	$(if $(PLAYER179),CPPFLAGS="$(CPPFLAGS) -DPLAYER179") \
 	$(if $(PLAYER191),CPPFLAGS="$(CPPFLAGS) -DPLAYER191") \
