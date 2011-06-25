5fe41bd5a7fc4e0c24564cf9fb7f5f50e4cd3c25
schischu <schischu@none.no>
2011-06-25 01:56:55 -0700

    [at7500] readding old flash routines for debugging purposes

---
+++ tdt/flash/at7500/scripts_extended/flash_part_fw.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+CURDIR=$1
+TUFSBOXDIR=$2
+OUTDIR=$3
+TMPFWDIR=$4
+
+echo "CURDIR     = $CURDIR"
+echo "TUFSBOXDIR = $TUFSBOXDIR"
+echo "OUTDIR     = $OUTDIR"
+echo "TMPFWDIR   = $TMPFWDIR"
+
+MKFSJFFS2=$TUFSBOXDIR/host/bin/mkfs.jffs2
+SUMTOOL=$TUFSBOXDIR/host/bin/sumtool
+PAD=$CURDIR/../common/pad
+FUP=$CURDIR/fup
+
+OUTFILE=$OUTDIR/update_fw.ird
+
+if [ ! -e $OUTDIR ]; then
+  mkdir $OUTDIR
+fi
+
+if [ -e $OUTFILE ]; then
+  rm -f $OUTFILE
+fi
+
+# Create a jffs2 partition for fw's
+# Size 6.875  MB = -p0x6E0000
+# Folder which contains fw's is -r fw
+# e.g.
+# .
+# ./fw
+# ./fw/audio.elf
+# ./fw/video.elf
+$MKFSJFFS2 -qUfv -p0x6E0000 -e0x20000 -r $TMPFWDIR -o $CURDIR/mtd_fw.bin
+$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_fw.bin -o $CURDIR/mtd_fw.sum.bin
+$PAD 0x6E0000 $CURDIR/mtd_fw.sum.bin $CURDIR/mtd_fw.sum.pad.bin
+
+# Create a fortis signed update file for fw's 
+echo "CMD: $FUP -ce $OUTFILE -f $CURDIR/mtd_fw.sum.pad.bin"
+$FUP -ce $OUTFILE -f $CURDIR/mtd_fw.sum.pad.bin
+
+rm -f $CURDIR/mtd_fw.bin
+rm -f $CURDIR/mtd_fw.sum.bin
+rm -f $CURDIR/mtd_fw.sum.pad.bin
+
+zip $OUTFILE.zip $OUTFILE
