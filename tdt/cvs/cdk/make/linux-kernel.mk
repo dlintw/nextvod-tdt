8b7e4a38b916fb5b9282e887e85130e83976a936
oxygen-1 <rene.fuerst@live.com>
2011-06-30 00:55:32 +0200

    [ufs910] stm24 207 working

--- tdt/cvs/cdk/make/linux-kernel.mk
+++ tdt/cvs/cdk/make/linux-kernel.mk
@@ -340,14 +340,13 @@ TF7700PATCHES_24 = $(COMMONPATCHES_24) \
 		linux-sh4-i2c-st40-pio_stm24_0205.patch
 
 UFS910PATCHES_24 = $(COMMONPATCHES_24) \
-			stx7100_fdma_fix_stm24_205.patch \
-			sata_32bit_fix_stm24_205.patch \
+			stx7100_fdma_fix_stm24$(PATCH_STR).patch \
+			sata_32bit_fix_stm24$(PATCH_STR).patch \
 			linux-sh4-ufs910_setup_stm24$(PATCH_STR).patch \
 			linux-usbwait123_stm24.patch \
 			linux-sh4-ufs910_reboot_stm24.patch \
-			linux-sh4-smsc911x_dma_stm24_p205.patch \
-			linux-sh4-stmmac_stm24_0205.patch \
-			linux-sh4-i2c-st40-pio_stm24_0205.patch
+			linux-sh4-smsc911x_dma_stm24$(PATCH_STR).patch \
+			linux-sh4-i2c-st40-pio_stm24$(PATCH_STR).patch
 
 FLASHUFS910PATCHES_24 = $(UFS910PATCHES_24)
 
