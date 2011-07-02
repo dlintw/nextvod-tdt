0c2e75a8af547b11c5a577b0707302f455379ffa
oxygen-1 <rene.fuerst@live.com>
2011-07-02 14:50:35 +0200

    [stm24 205/207] add some patches from the st git

--- tdt/cvs/cdk/make/linux-kernel.mk
+++ tdt/cvs/cdk/make/linux-kernel.mk
@@ -332,7 +332,9 @@ COMMONPATCHES_24 = \
 		linux-sh4-strcpy_stm24$(PATCH_STR).patch \
 		linux-squashfs-lzma_stm24$(PATCH_STR).patch \
 		linux-sh4-ext23_as_ext4_stm24$(PATCH_STR).patch \
-		bpa2_procfs_stm24$(PATCH_STR).patch
+		bpa2_procfs_stm24$(PATCH_STR).patch \
+		xchg_fix_stm24$(PATCH_STR).patch \
+		mm_cache_update_stm24$(PATCH_STR).patch
 
 TF7700PATCHES_24 = $(COMMONPATCHES_24) \
 		linux-sh4-tf7700_setup_stm24$(PATCH_STR).patch \
