d811fc55808485e509feca6746a9db441f905588
spider <spworld2008@gmail.com>
2011-11-17 23:02:02 +0200

    [spark] release.mk

--- tdt/cvs/cdk/make/release.mk
+++ tdt/cvs/cdk/make/release.mk
@@ -270,9 +270,10 @@ release_spark7162:
 	cp $(targetprefix)/boot/video_7105.elf $(prefix)/release/boot/video.elf
 	cp $(targetprefix)/boot/audio_7105.elf $(prefix)/release/boot/audio.elf
 	cp -f $(buildprefix)/root/usr/local/share/enigma2/keymap_spark.xml $(prefix)/release/usr/local/share/enigma2/keymap.xml
-	##cp -f $(buildprefix)/root/release/vfd_spark7162$(KERNELSTMLABEL).ko $(prefix)/release/lib/modules/vfd.ko
-	cp $(targetprefix)/lib/modules/$(KERNELVERSION)/extra/frontcontroller/vfd.fulan/vfd.ko $(prefix)/release/lib/modules
+	cp -f $(buildprefix)/root/release/vfd_spark7162$(KERNELSTMLABEL).ko $(prefix)/release/lib/modules/vfd.ko
 	cp -f $(buildprefix)/root/release/tuner.ko$(KERNELSTMLABEL)_spark7162 $(prefix)/release/lib/modules/spark7162.ko
+	cp -f $(buildprefix)/root/sbin/flash_* $(prefix)/release/sbin
+	cp -f $(buildprefix)/root/sbin/nand* $(prefix)/release/sbin
 
 	cp -dp $(buildprefix)/root/etc/lircd_spark7162.conf $(prefix)/release/etc/lircd.conf
 	cp -p $(targetprefix)/usr/bin/lircd $(prefix)/release/usr/bin/
