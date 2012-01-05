0bef76406e28e7aec5054719d8c5953ee182b351
max10 <max_10@gmx.de>
2012-01-05 16:58:24 +0100

    add archivedir step11

--- tdt/cvs/cdk/make/tf_u-boot.mk
+++ tdt/cvs/cdk/make/tf_u-boot.mk
@@ -1,15 +1,15 @@
 #
 # U-BOOT for the TF7700
 #
-HOST_U_BOOT	:= host-u-boot
-U_BOOT_RAWVERSION	:= 1.3.1_stm23_0043
-U_BOOT_VERSION	:= $(U_BOOT_RAWVERSION)-43
-U_BOOT_DIR	:= u-boot/u-boot-sh4-$(U_BOOT_RAWVERSION)
+HOST_U_BOOT := host-u-boot
+U_BOOT_RAWVERSION := 1.3.1_stm23_0043
+U_BOOT_VERSION := $(U_BOOT_RAWVERSION)-43
+U_BOOT_DIR := u-boot/u-boot-sh4-$(U_BOOT_RAWVERSION)
 U_BOOT_PREFIX	:= stlinux23-$(HOST_U_BOOT)-source-sh4
 TFINSTALLER_DIR := tfinstaller
 
 RPMS/noarch/$(U_BOOT_PREFIX)-$(U_BOOT_VERSION).noarch.rpm: \
-		Archive/$(U_BOOT_PREFIX)-$(U_BOOT_VERSION).src.rpm
+		$(archivedir)/$(U_BOOT_PREFIX)-$(U_BOOT_VERSION).src.rpm
 	rpm $(DRPM) --nosignature -Uhv $< && \
 	mv SPECS/stm-$(HOST_U_BOOT).spec SPECS/stm-$(HOST_U_BOOT).spec_ && \
 	sed -e "s/if_target_cpu sh/if 1/g" SPECS/stm-$(HOST_U_BOOT).spec_ > SPECS/stm-$(HOST_U_BOOT).spec && \
@@ -70,11 +70,11 @@ $(DEPDIR)/u-boot: $(DEPDIR)/u-boot.do_compile $(TFINSTALLER_DIR)/u-boot.ftfd
 #
 # HOST-U-BOOT-TOOLS
 #
-HOST_U_BOOT_TOOLS	:= host-u-boot-tools
-U_BOOT_TOOLS_VERSION	:= 1.3.1_stm23-7
+HOST_U_BOOT_TOOLS := host-u-boot-tools
+U_BOOT_TOOLS_VERSION := 1.3.1_stm23-7
 
 RPMS/sh4/stlinux23-$(HOST_U_BOOT_TOOLS)-$(U_BOOT_TOOLS_VERSION).sh4.rpm: \
-		Archive/stlinux23-$(HOST_U_BOOT_TOOLS)-$(U_BOOT_TOOLS_VERSION).src.rpm
+		$(archivedir)/stlinux23-$(HOST_U_BOOT_TOOLS)-$(U_BOOT_TOOLS_VERSION).src.rpm
 	rpm $(DRPM) --nosignature -Uhv $< && \
 	rpmbuild $(DRPMBUILD) -bb -v --clean --target=sh4-linux --define "_stm_short_build_id 23" --define "_stm_pkg_prefix stlinux23" --define "_stm_uboot_dir $(BUILDPREFIX)/u-boot" SPECS/stm-$(HOST_U_BOOT_TOOLS).spec
 
