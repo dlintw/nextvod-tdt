dc77c40b12e44f58c069f1dc86d6b3d26356d993
max10 <max_10@gmx.de>
2012-01-05 17:08:24 +0100

    add archivedir step14

--- tdt/cvs/cdk/rules-archive.pl
+++ tdt/cvs/cdk/rules-archive.pl
@@ -15,44 +15,44 @@ while ( <RULES> )
     @_ = split ( /;/, $_ );
     my $file = $_[0];
     my $filerep = $_[0];
-    $head .= " Archive/" . $file;
-    $output .= "Archive/" . $file . ":\n\tfalse";
+    $head .= " \$(archivedir)/" . $file;
+    $output .= " \$(archivedir)/" . $file . ":\n\tfalse";
     shift @_;
     foreach ( @_ )
     {
       if ( $_ =~ m#^ftp://# )
       {
-        $output .= " || \\\n\twget -c --passive-ftp -P Archive " . $_ . "/" . $file;
+        $output .= " || \\\n\twget -c --passive-ftp -P \$(archivedir) " . $_ . "/" . $file;
       }
       elsif ( $_ =~ m#^http://# )
       {
-        $output .= " || \\\n\twget -c -P Archive " . $_ . "/" . $file;
+        $output .= " || \\\n\twget -c -P \$(archivedir) " . $_ . "/" . $file;
       }
       elsif ( $_ =~ m#^CMD_CVS # )
       {
-        $output .= " || \\\n\tcd Archive && " . $_;
+        $output .= " || \\\n\tcd \$(archivedir) && " . $_;
         my $cvsstring = $_;
         $cvsstring =~ s/ co / up /;
-        $outputupdate .= "Archive/" . $file . "up:\n\tfalse";
-        $outputupdate .= " || \\\n\tcd Archive && " . $cvsstring;
+        $outputupdate .= "\$(archivedir)" . $file . "up:\n\tfalse";
+        $outputupdate .= " || \\\n\tcd \$(archivedir) && " . $cvsstring;
       }
     }
     if ( $file =~ m/gz$/ )
     {
-      $output .= " || \\\n\twget -c -P Archive ftp://ftp.stlinux.com/pub/stlinux/2.0/ST_Linux_2.0/RPM_Distribution/sh4-target-glibc-packages/" . $file;
+      $output .= " || \\\n\twget -c -P \$(archivedir) ftp://ftp.stlinux.com/pub/stlinux/2.0/ST_Linux_2.0/RPM_Distribution/sh4-target-glibc-packages/" . $file;
       $output .= "\n\t\@touch \$\@";
     }
     elsif ( $file =~ m/cvs$/ )
     {
       $output .= " || \\\n\twget -c ftp://xxx.com/pub/tufsbox/cdk/src/" . $file;
       $filerep =~ s/\.cvs//;
-      $output .= "\n\t\@touch -r Archive/" . $filerep . "/CVS \$\@";
-      $outputupdate .= "\n\t\@touch -r Archive/" . $filerep . "/CVS \$\(subst cvsup,cvs,\$\@\)";
+      $output .= "\n\t\@touch -r \$(archivedir) " . $filerep . "/CVS \$\@";
+      $outputupdate .= "\n\t\@touch -r \$(archivedir) " . $filerep . "/CVS \$\(subst cvsup,cvs,\$\@\)";
       $outputupdate .= "\n\n";
     }
     else
     {
-      $output .= " || \\\n\twget -c -P Archive http://tuxbox.berlios.de/pub/tuxbox/cdk/src/" . $file;
+      $output .= " || \\\n\twget -c -P \$(archivedir) http://tuxbox.berlios.de/pub/tuxbox/cdk/src/" . $file;
       $output .= "\n\t\@touch \$\@";
     }
     $output .= "\n\n";
