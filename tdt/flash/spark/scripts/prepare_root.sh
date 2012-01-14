ca333638b5dd1d6419d8c639a5b18ee175f9a4fa
Schischu <Schischu@home>
2012-01-14 07:47:33 -0800

    [spark-flash] generate dev nodes for flash image

--- tdt/flash/spark/scripts/prepare_root.sh
+++ tdt/flash/spark/scripts/prepare_root.sh
@@ -12,8 +12,22 @@ cp -a $RELEASEDIR/* $TMPROOTDIR
 mv $TMPROOTDIR/boot/uImage $TMPKERNELDIR/uImage
 
 
-# --- ROOT ---
-if [  -e $CURDIR/extras/dev_ufs910.tar.gz ]; then
-  sudo tar -xzf $CURDIR/extras/dev_ufs910.tar.gz -C $TMPROOTDIR/dev/
-fi
+# --- ROOT ---i
+cd $TMPROOTDIR/dev/
+MAKEDEV="sudo $TMPROOTDIR/sbin/MAKEDEV -p $TMPROOTDIR/etc/passwd -g $TMPROOTDIR/etc/group"
+${MAKEDEV} std
+${MAKEDEV} fd
+${MAKEDEV} hda hdb
+${MAKEDEV} sda sdb sdc sdd
+${MAKEDEV} scd0 scd1
+${MAKEDEV} st0 st1
+${MAKEDEV} sg
+${MAKEDEV} ptyp ptyq
+${MAKEDEV} console
+${MAKEDEV} ttyAS0 ttyAS1 ttyAS2 ttyAS3
+${MAKEDEV} lp par audio video fb rtc lirc st200 alsasnd mme bpamem
+${MAKEDEV} ppp busmice
+${MAKEDEV} input i2c mtd
+${MAKEDEV} dvb
+cd -
 
