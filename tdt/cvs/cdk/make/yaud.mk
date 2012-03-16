435dcc461cfd61046cba2fa7cf9963808a65e04e
max10 <max_10@gmx.de>
2012-03-16 17:03:36 +0100

    rename neutrino-nightly -> neutrino-beta

--- tdt/cvs/cdk/make/yaud.mk
+++ tdt/cvs/cdk/make/yaud.mk
@@ -32,7 +32,6 @@ min-bare-os std-bare-os max-bare-os bare-os: \
 		%busybox \
 		\
 		%libz \
-		%grep \
 		%$(INITSCRIPTS) \
 		%openrdate \
 		%$(NETBASE) \
@@ -41,8 +40,6 @@ min-bare-os std-bare-os max-bare-os bare-os: \
 		%$(DISTRIBUTIONUTILS) \
 		\
 		%e2fsprogs \
-		%jfsutils \
-		%opkg \
 		%u-boot-utils \
 		%diverse-tools
 #		%$(RELEASE) \
@@ -50,19 +47,22 @@ min-bare-os std-bare-os max-bare-os bare-os: \
 #
 
 min-net-utils std-net-utils max-net-utils net-utils: \
-%net-utils:	\
+%net-utils: \
 		%$(NETKIT_FTP) \
 		%autofs \
 		%portmap \
 		%$(NFSSERVER) \
 		%vsftpd \
 		%ethtool \
+		%opkg \
+		%grep \
 		%$(CIFS)
 
 min-disk-utils std-disk-utils max-disk-utils disk-utils: \
-%disk-utils:	\
+%disk-utils: \
 		%$(XFSPROGS) \
 		%util-linux \
+		%jfsutils \
 		%$(SG3)
 
 #dummy targets
@@ -80,7 +80,6 @@ max-:
 yaud-stock: yaud-none stock
 	@TUXBOX_YAUD_CUSTOMIZE@
 
-#yaud-enigma: yaud-none splashutils enigma
 yaud-enigma: yaud-none lirc stslave \
 		boot-elf misc-cp remote firstboot enigma
 	@TUXBOX_YAUD_CUSTOMIZE@
@@ -101,8 +100,8 @@ yaud-neutrino-static: yaud-none lirc stslave\
 		boot-elf remote firstboot neutrino release_neutrino_static
 	@TUXBOX_YAUD_CUSTOMIZE@
 
-yaud-neutrino-nightly: yaud-none lirc stslave\
-		boot-elf remote firstboot neutrino-nightly release_neutrino_nightly
+yaud-neutrino-beta: yaud-none lirc stslave\
+		boot-elf remote firstboot neutrino-beta release_neutrino_nightly
 	@TUXBOX_YAUD_CUSTOMIZE@
 
 flash-ufs910-neutrino: yaud-none stslave \
@@ -143,7 +142,8 @@ yaud-enigma1-hd: yaud-none lirc stslave\
 endif
 
 #work yet!!!
-flash-ufs910-enigma2: yaud-none host-python lirc stslave \
+flash-ufs910-enigma2: \
+		yaud-none host-python lirc stslave \
 		boot-elf hotplug remote firstboot enigma2 enigma2-misc release \
 		kernel-squashfs.ubimage \
 		conf-stock.enigma2 \
@@ -152,14 +152,16 @@ flash-ufs910-enigma2: yaud-none host-python lirc stslave \
 		var-stock.enigma2
 	@TUXBOX_YAUD_CUSTOMIZE@
 
-flash-hdbox-enigma2: yaud-none host-python lirc \
+flash-hdbox-enigma2: \
+		yaud-none host-python lirc \
 		boot-elf hotplug remote firstboot enigma2 enigma2-misc release \
 		kernel-squashfs-hdbox.ubimage \
 		root-stock-hdbox.enigma2 \
 		var-stock-hdbox.enigma2
 	@TUXBOX_YAUD_CUSTOMIZE@
 
-yaud-none:	bare-os \
+yaud-none: \
+		bare-os \
 		libdvdcss \
 		libdvdread \
 		libdvdnav \
@@ -193,7 +195,6 @@ min-yaud-none: \
 #
 #min-yaud-none: \
 #%yaud-none: %bare-os \
-#		...
 #		%$(RELEASE) \
 #		%linux-kernel \
 #		%busybox \
@@ -211,7 +212,8 @@ std-yaud-stock: \
 	@TUXBOX_YAUD_CUSTOMIZE@
 
 std-yaud-none: \
-%yaud-none:	%bare-os \
+%yaud-none: \
+		%bare-os \
 		%misc-tools \
 		%linux-kernel \
 		%net-utils \
@@ -228,7 +230,8 @@ max-yaud-stock: \
 	@TUXBOX_YAUD_CUSTOMIZE@
 
 max-yaud-none: \
-%yaud-none: 	%bare-os \
+%yaud-none: \
+		%bare-os \
 		%misc-tools \
 		%$(UDEV) \
 		%$(HOTPLUG) \
