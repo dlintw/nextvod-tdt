dae41e9bd174f65f80cad6f4a53d53233625106b
max10 <max_10@gmx.de>
2012-03-16 16:34:28 +0100

    fix patchdir path

--- tdt/cvs/cdk/make/hostapps.mk
+++ tdt/cvs/cdk/make/hostapps.mk
@@ -47,9 +47,9 @@ $(hostprefix)/bin/mksquashfs: @DEPENDS_squashfs@
 	mkdir -p @DIR_squashfs@
 	cd @DIR_squashfs@ && \
 	bunzip2 -cd $(archivedir)/lzma442.tar.bz2 | TAPE=- tar -x && \
-	patch -p1 < ../Patches/lzma_zlib-stream.diff && \
+	patch -p1 < $(buildprefix)/Patches/lzma_zlib-stream.diff && \
 	gunzip -cd $(archivedir)/squashfs3.0.tar.gz | TAPE=- tar -x && \
-	cd squashfs3.0 && patch -p1 < ../../Patches/mksquashfs_lzma.diff
+	cd squashfs3.0 && patch -p1 < $(buildprefix)/Patches/mksquashfs_lzma.diff
 	$(MAKE) -C @DIR_squashfs@/C/7zip/Compress/LZMA_Lib
 	$(MAKE) -C @DIR_squashfs@/squashfs3.0/squashfs-tools
 	$(INSTALL) -d $(@D)
@@ -63,7 +63,7 @@ $(hostprefix)/bin/mksquashfs: @DEPENDS_squashfs@
 	cd @DIR_squashfs@ && \
 	bunzip2 -cd $(archivedir)/lzma465.tar.bz2 | TAPE=- tar -x && \
 	gunzip -cd $(archivedir)/squashfs4.0.tar.gz | TAPE=- tar -x && \
-	cd squashfs4.0/squashfs-tools && patch -p1 < ../../../Patches/squashfs-tools-4.0-lzma.patch
+	cd squashfs4.0/squashfs-tools && patch -p1 < $(buildprefix)/Patches/squashfs-tools-4.0-lzma.patch
 	$(MAKE) -C @DIR_squashfs@/squashfs4.0/squashfs-tools
 	$(INSTALL) -d $(@D)
 	$(INSTALL) -m755 @DIR_squashfs@/squashfs4.0/squashfs-tools/mksquashfs $@
