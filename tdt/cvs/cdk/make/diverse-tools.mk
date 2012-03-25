413e7d5eafc05fe276d7498543143d927d3945cb
max10 <max_10@gmx.de>
2012-03-25 12:45:05 +0200

    remove deprecated old targets as announced step11

--- tdt/cvs/cdk/make/diverse-tools.mk
+++ tdt/cvs/cdk/make/diverse-tools.mk
@@ -21,24 +21,3 @@ $(DEPDIR)/%diverse-tools: $(DIVERSE_TOOLS_ADAPTED_ETC_FILES:%=root/etc/%)
 #	$(INSTALL_BIN) root/bin/devinit $(prefix)/$*cdkroot/bin
 	[ "x$*" = "x" ] && touch $@ || true
 	@TUXBOX_YAUD_CUSTOMIZE@
-
-flash-diverse-tools: $(flashprefix)/root/etc/init.d/allerlei
-
-$(flashprefix)/root/etc/init.d/allerlei: $(DIVERSE_TOOLS_ADAPTED_ETC_FILES:%=root/etc/%)
-	( cd root/etc && for i in $(DIVERSE_TOOLS_ADAPTED_ETC_FILES); do \
-		[ -f $$i ] && $(INSTALL) -m644 $$i $(flashprefix)/root/etc/$$i || true; \
-		[ "$${i%%/*}" = "init.d" ] && chmod 755 $(flashprefix)/root/etc/$$i || true; done ) && \
-	( export HHL_CROSS_TARGET_DIR=$(flashprefix)/root && cd $(flashprefix)/root/etc/init.d && \
-		for s in $(DIVERSE_TOOLS_ADAPTED_ETC_FILES) ; do \
-			[ "$${s%%/*}" = "init.d" ] && ( \
-			$(hostprefix)/bin/target-initdconfig --add $${s#init.d/} || \
-			echo "Unable to enable initd service: $${s#init.d/}" ) ; done && rm *rpmsave 2>/dev/null || true ) && \
-	ln -sf /usr/share/zoneinfo/Europe/Berlin $(flashprefix)/root/etc/localtime
-#	( \
-#		[ -f root/usr/bin/dbox-perf-hdd.sh ] && $(INSTALL) -m 755 root/usr/bin/dbox-perf-hdd.sh $(flashprefix)/root/usr/bin || true && \
-#		[ -f root/usr/bin/dbox-perf-nfs.sh ] && $(INSTALL) -m 755 root/usr/bin/dbox-perf-nfs.sh $(flashprefix)/root/usr/bin || true )
-	$(INSTALL_BIN) root/usr/sbin/mountro $(flashprefix)/root/usr/sbin/
-	$(INSTALL_BIN) root/usr/sbin/mountrw $(flashprefix)/root/usr/sbin/
-	$(INSTALL_BIN) root/bin/devinit $(flashprefix)/root/bin
-	touch $@
-	@FLASHROOTDIR_MODIFIED@
