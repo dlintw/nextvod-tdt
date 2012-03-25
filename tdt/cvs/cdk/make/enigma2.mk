85f5a4f81cbb5240952360005e7d60410209ceda
max10 <max_10@gmx.de>
2012-03-25 12:49:29 +0200

    remove deprecated old targets as announced step12

--- tdt/cvs/cdk/make/enigma2.mk
+++ tdt/cvs/cdk/make/enigma2.mk
@@ -105,34 +105,6 @@ enigma2-clean enigma2-distclean:
 	cd $(appsdir)/enigma2 && \
 		$(MAKE) distclean
 
-flash-enigma2: $(flashprefix)/root-enigma2
-
-$(flashprefix)/root-enigma2: \
-		bootstrap $(wildcard root-enigma2-local.sh) \
-		$(ENIGMA2_GUI_ADAPTED_ETC_FILES:%=root/etc/%) \
-		enigma2.do_compile
-	$(MAKE) flash-python-enigma2 && \
-	$(MAKE) flash-libxml2-enigma2 && \
-	$(MAKE) flash-libxslt-enigma2 && \
-	$(MAKE) flash-elementtree-enigma2 && \
-	$(MAKE) flash-zope-interface-enigma2 && \
-	$(MAKE) flash-twisted-enigma2 && \
-	$(MAKE) flash-libxmlccwrap-enigma2 && \
-	$(MAKE) -C $(appsdir)/enigma2 install DESTDIR=$@ && \
-	$(MAKE) enigma2-misc targetprefix=$@ && \
-	$(INSTALL_DIR) $@/etc/init.d && \
-	$(INSTALL_BIN) root/etc/init.d/{player,enigma2} $@/etc/init.d && \
-	$(INSTALL_DIR) $@/etc/rc.d/rc{S,0,1,2,3,4,5,6}.d && \
-	$(LN_SF) ../init.d $@/etc/rc.d/init.d && \
-	( export HHL_CROSS_TARGET_DIR=$@ && cd $@/etc/init.d && \
-		for s in player enigma2 ; do \
-			$(hostprefix)/bin/target-initdconfig --add $$s || \
-			echo "Unable to enable initd service: $$s" ; done && rm *rpmsave 2>/dev/null || true ) && \
-	$(INSTALL_BIN) root/etc/init.d/rcS_flash $@/etc/init.d/rcS && \
-	$(INSTALL_BIN) root/etc/init.d/rc $@/etc/init.d/ && \
-	touch $@
-	@TUXBOX_CUSTOMIZE@
-
 #
 # dvb/libdvbsi++
 #
