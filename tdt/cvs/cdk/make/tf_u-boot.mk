e74e7d691eafacfcd4bdc5be83ee98f85046b62b
max10 <max_10@gmx.de>
2012-03-25 20:53:45 +0200

    fix tf7700 uboot.mk

--- tdt/cvs/cdk/make/tf_u-boot.mk
+++ tdt/cvs/cdk/make/tf_u-boot.mk
@@ -14,9 +14,9 @@ RPMS/noarch/$(U_BOOT_PREFIX)-$(U_BOOT_VERSION).noarch.rpm: \
 	mv SPECS/stm-$(HOST_U_BOOT).spec SPECS/stm-$(HOST_U_BOOT).spec_ && \
 	sed -e "s/if_target_cpu sh/if 1/g" SPECS/stm-$(HOST_U_BOOT).spec_ > SPECS/stm-$(HOST_U_BOOT).spec && \
 	rm SPECS/stm-$(HOST_U_BOOT).spec_ && \
-	rpmbuild $(DRPMBUILD) -bb -v --clean --define "_stm_short_build_id 23" --define "_stm_target_name sh4" --define "_stm_pkg_prefix stlinux23" --target=sh --define "_stm_uboot_dir $(BUILDPREFIX)/u-boot" SPECS/stm-$(HOST_U_BOOT).spec
+	rpmbuild $(DRPMBUILD) -bb -v --clean --define "_stm_short_build_id 23" --define "_stm_target_name sh4" --define "_stm_pkg_prefix stlinux23" --target=sh --define "_stm_uboot_dir $(buildprefix)/u-boot" SPECS/stm-$(HOST_U_BOOT).spec
 
-$(DEPDIR)/u-boot.do_prepare:  RPMS/noarch/$(U_BOOT_PREFIX)-$(U_BOOT_VERSION).noarch.rpm $(BUILDPREFIX)/Patches/u-boot-1.3.1_stm23_0043_tf7700.patch
+$(DEPDIR)/u-boot.do_prepare:  RPMS/noarch/$(U_BOOT_PREFIX)-$(U_BOOT_VERSION).noarch.rpm $(buildprefix)/Patches/u-boot-1.3.1_stm23_0043_tf7700.patch
 # FIXME: "rpm -e" does not remove files
 	rm -fr $(U_BOOT_DIR)
 	@rpm $(DRPM) --ignorearch --nodeps -Uhv $< && \
@@ -64,7 +64,7 @@ U_BOOT_TOOLS_VERSION := 1.3.1_stm23-7
 RPMS/sh4/stlinux23-$(HOST_U_BOOT_TOOLS)-$(U_BOOT_TOOLS_VERSION).sh4.rpm: \
 		$(archivedir)/stlinux23-$(HOST_U_BOOT_TOOLS)-$(U_BOOT_TOOLS_VERSION).src.rpm
 	rpm $(DRPM) --nosignature -Uhv $< && \
-	rpmbuild $(DRPMBUILD) -bb -v --clean --target=sh4-linux --define "_stm_short_build_id 23" --define "_stm_pkg_prefix stlinux23" --define "_stm_uboot_dir $(BUILDPREFIX)/u-boot" SPECS/stm-$(HOST_U_BOOT_TOOLS).spec
+	rpmbuild $(DRPMBUILD) -bb -v --clean --target=sh4-linux --define "_stm_short_build_id 23" --define "_stm_pkg_prefix stlinux23" --define "_stm_uboot_dir $(buildprefix)/u-boot" SPECS/stm-$(HOST_U_BOOT_TOOLS).spec
 
 $(DEPDIR)/$(HOST_U_BOOT_TOOLS): u-boot.do_prepare RPMS/sh4/stlinux23-$(HOST_U_BOOT_TOOLS)-$(U_BOOT_TOOLS_VERSION).sh4.rpm | bootstrap-cross
 	@rpm $(DRPM) --ignorearch --nodeps -Uhv $(lastword $^) && \
