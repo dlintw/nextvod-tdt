dfc1506a4fc87ad1cf7ce905b55ffd8a875a9ca0
BPanther <bpanther_ts@hotmail.com>
2012-03-31 23:26:55 +0200

    better fix, thx max

--- tdt/cvs/cdk/make/linux-kernel.mk
+++ tdt/cvs/cdk/make/linux-kernel.mk
@@ -477,10 +477,8 @@ TF7700PATCHES_24 = $(COMMONPATCHES_24) \
 		linux-sh4-sata-v06_stm24$(PATCH_STR).patch
 
 UFS910PATCHES_24 = $(COMMONPATCHES_24) \
-		$(if $(P0209),linux-sh4-mmap_stm24.patch) \
-		$(if $(P0210),linux-sh4-mmap_stm24.patch) \
-		$(if $(P0209),linux-sh4-remove_pcm_reader_stm24.patch) \
-		$(if $(P0210),linux-sh4-remove_pcm_reader_stm24.patch) \
+		$(if $(P0209)$(P0210),linux-sh4-mmap_stm24.patch) \
+		$(if $(P0209)$(P0210),linux-sh4-remove_pcm_reader_stm24.patch) \
 		stx7100_fdma_fix_stm24$(PATCH_STR).patch \
 		sata_32bit_fix_stm24$(PATCH_STR).patch \
 		sata_stx7100_B4Team_fix_stm24$(PATCH_STR).patch \
