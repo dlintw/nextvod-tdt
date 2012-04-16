243cebfa9c7a59b76070da48c0fd77f3a63b72dd
max10 <max_10@gmx.de>
2012-04-16 20:57:26 +0200

    update host-filesystem for SUSE 64bit

--- tdt/cvs/cdk/make/bootstrap.mk
+++ tdt/cvs/cdk/make/bootstrap.mk
@@ -86,6 +86,7 @@ host-filesystem:
 	$(INSTALL) -d $(devkitprefix)/sources/kernel
 	$(INSTALL) -d $(hostprefix)
 	$(INSTALL) -d $(hostprefix)/{bin,doc,etc,include,info,lib,man,share,var}
+	ln -s /$(hostprefix)/lib $(hostprefix)/lib64
 	$(INSTALL) -d $(hostprefix)/man/man{1,2,3,4,5,6,7,8,9}
 	touch .deps/$@
 
