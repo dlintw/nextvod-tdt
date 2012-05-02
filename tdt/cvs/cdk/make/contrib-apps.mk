ab9e78770b009880246b6f1c74cd04532f3f6103
max10 <max_10@gmx.de>
2012-05-02 13:56:44 +0200

    Revert "fix typo e2fsprogs"
    This reverts commit 2fc476732ce204bb632938ee0e846ba3d38a21f6.

--- tdt/cvs/cdk/make/contrib-apps.mk
+++ tdt/cvs/cdk/make/contrib-apps.mk
@@ -154,7 +154,7 @@ $(DEPDIR)/e2fsprogs.do_compile: $(DEPDIR)/e2fsprogs.do_prepare | $(UTIL_LINUX)
 	cd @DIR_e2fsprogs@ && \
 	$(BUILDENV) \
 	CFLAGS="$(TARGET_CFLAGS) -Os" \
-	cc="$(target)-gcc" \
+	cc=$(target)-gcc \
 	./configure \
 		--build=$(build) \
 		--host=$(target) \
@@ -175,7 +175,7 @@ else !STM24
 $(DEPDIR)/e2fsprogs.do_compile: $(DEPDIR)/e2fsprogs.do_prepare
 	cd @DIR_e2fsprogs@ && \
 	$(BUILDENV) \
-	cc="$(target)-gcc" \
+	cc=$(target)-gcc \
 	./configure \
 		--build=$(build) \
 		--host=$(target) \
