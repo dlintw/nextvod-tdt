ae39b7b50b06a96559744be33cfaa85bb732452e
root <root@duckbox.(none)>
2012-06-17 08:15:53 +0200

    [spark] flash - padding of kernel uImage is not needed, and pack zip without directories structure

--- tdt/flash/spark/scripts/flash_part_w_fw.sh
+++ tdt/flash/spark/scripts/flash_part_w_fw.sh
@@ -29,8 +29,8 @@ fi
 
 # --- KERNEL ---
 # Size 8MB !
-cp $TMPKERNELDIR/uImage $CURDIR/uImage
-$PAD 0x800000 $CURDIR/uImage $CURDIR/mtd_kernel.pad.bin
+cp -f $TMPKERNELDIR/uImage $OUTDIR/uImage
+#$PAD 0x800000 $CURDIR/uImage $CURDIR/mtd_kernel.pad.bin #padding of kernel uImage is not needed
 
 # --- ROOT ---
 # Size 64MB !
@@ -46,15 +46,15 @@ $PAD 0x4000000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin
 #echo "SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.pad.bin"
 #$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.pad.bin
 
-rm -f $CURDIR/uImage
+#rm -f $CURDIR/uImage
 rm -f $CURDIR/mtd_root.bin
 rm -f $CURDIR/mtd_root.sum.bin
 
-SIZE=`stat mtd_kernel.pad.bin -t --format %s`
-SIZE=`printf "0x%x" $SIZE`
-if [[ $SIZE > "0x800000" ]]; then
-  echo "KERNEL TO BIG. $SIZE instead of 0x800000" > /dev/stderr
-fi
+#SIZE=`stat mtd_kernel.pad.bin -t --format %s`
+#SIZE=`printf "0x%x" $SIZE`
+#if [[ $SIZE > "0x800000" ]]; then
+#  echo "KERNEL TO BIG. $SIZE instead of 0x800000" > /dev/stderr
+#fi
 
 SIZE=`stat mtd_root.sum.pad.bin -t --format %s`
 SIZE=`printf "0x%x" $SIZE`
@@ -62,10 +62,10 @@ if [[ $SIZE > "0x4000000" ]]; then
   echo "ROOT TO BIG. $SIZE instead of 0x4000000" > /dev/stderr
 fi
 
-mv $CURDIR/mtd_kernel.pad.bin $OUTDIR/uImage
+#mv $CURDIR/mtd_kernel.pad.bin $OUTDIR/uImage
 mv $CURDIR/mtd_root.sum.pad.bin $OUTDIR/e2jffs2.img
 
 rm -f $CURDIR/mtd_kernel.pad.bin
 rm -f $CURDIR/mtd_root.sum.pad.bin
 
-zip $OUTFILE.zip $OUTDIR/e2jffs2.img $OUTDIR/uImage
+cd $OUTDIR;zip $OUTFILE.zip e2jffs2.img uImage
