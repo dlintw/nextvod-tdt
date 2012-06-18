bbba30cbfd4fd2efadb0dd1c0ead212c362a6f73
max10 <max_10@gmx.de>
2012-06-18 17:09:18 +0200

    fix ImageMagicks CFLAGS

--- tdt/cvs/cdk/make/contrib-apps.mk
+++ tdt/cvs/cdk/make/contrib-apps.mk
@@ -889,6 +889,8 @@ $(DEPDIR)/imagemagick.do_prepare: bootstrap @DEPENDS_imagemagick@
 $(DEPDIR)/imagemagick.do_compile: $(DEPDIR)/imagemagick.do_prepare
 	cd @DIR_imagemagick@ && \
 	$(BUILDENV) \
+	CFLAGS="-O1" \
+	PKG_CONFIG=$(hostprefix)/bin/pkg-config \
 	./configure \
 		--host=$(target) \
 		--prefix=/usr \
@@ -902,6 +904,7 @@ $(DEPDIR)/imagemagick.do_compile: $(DEPDIR)/imagemagick.do_prepare
 		--without-xml \
 		--without-perl \
 		--disable-openmp \
+		--disable-opencl
 		--without-zlib \
 		--enable-shared \
 		--enable-static \
