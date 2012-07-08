118823db7798331fdd88d17883cf42cc69599672
TangoCash <me@spam.dot.com>
2012-07-09 00:29:30 +0200

    NHD2: fix last commit

--- tdt/cvs/cdk/make/neutrino_hd2.mk
+++ tdt/cvs/cdk/make/neutrino_hd2.mk
@@ -20,10 +20,8 @@ N_CONFIG_OPTS = --enable-silent-rules
 #
 $(DEPDIR)/neutrino-hd2.do_prepare:
 	svn co http://neutrinohd2.googlecode.com/svn/trunk/neutrino-hd $(appsdir)/neutrino-hd2
-#	rm -rf $(appsdir)/neutrino-hd2/lib/libcoolstream/*.*
 	cp -ra $(appsdir)/neutrino-hd2 $(appsdir)/neutrino-hd2.org
 	cd $(appsdir)/neutrino-hd2 && patch -p1 < "$(buildprefix)/Patches/neutrino.hd2.diff"
-#	cd $(appsdir)/neutrino-hd2 && patch -p1 < "$(buildprefix)/Patches/neutrino.hd2.extra.diff"
 	touch $@
 
 $(appsdir)/neutrino-hd2/config.status: bootstrap $(EXTERNALLCD_DEP) freetype jpeg libpng libgif libid3tag curl libmad libvorbisidec libboost libflac openssl sdparm
