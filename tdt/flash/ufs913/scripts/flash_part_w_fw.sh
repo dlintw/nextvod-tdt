a1bc719cff9346d60ad3f219c23a4b3b6c551302
Schischu <schischu@duckbox>
2012-07-17 22:12:38 +0200

    [ufs913] flash preperations

---
+++ tdt/flash/ufs913/scripts/flash_part_w_fw.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+CURDIR=$1
+TUFSBOXDIR=$2
+OUTDIR=$3
+TMPKERNELDIR=$4
+TMPFWDIR=$5
+TMPROOTDIR=$6
+
+echo "CURDIR       = $CURDIR"
+echo "TUFSBOXDIR   = $TUFSBOXDIR"
+echo "OUTDIR       = $OUTDIR"
+echo "TMPKERNELDIR = $TMPKERNELDIR"
+echo "TMPFWDIR     = $TMPFWDIR"
+echo "TMPROOTDIR   = $TMPROOTDIR"
+
+MKFSJFFS2=$TUFSBOXDIR/host/bin/mkfs.jffs2
+SUMTOOL=$TUFSBOXDIR/host/bin/sumtool
+MUP=$CURDIR/mup
+
+OUTFILE=$OUTDIR/update_w_fw.img
+
+if [ ! -e $OUTDIR ]; then
+  mkdir $OUTDIR
+fi
+
+if [ -e $OUTFILE ]; then
+  rm -f $OUTFILE
+fi
+
+cp $TMPKERNELDIR/uImage $CURDIR/uImage
+
+# Create a jffs2 partition for fw's
+# Size 8mb = -p0x800000
+# Folder which contains fw's is -r fw
+# e.g.
+# .
+# ./fw
+# ./fw/audio.elf
+# ./fw/video.elf
+$MKFSJFFS2 -qUfv -p0x800000 -e0x20000 -r $TMPFWDIR -o $CURDIR/mtd_fw.bin
+$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_fw.bin -o $CURDIR/mtd_fw.sum.bin
+# Create a jffs2 partition for root
+# Size 64mb = -p0x4000000
+# Folder which contains fw's is -r fw
+# e.g.
+# .
+# ./release
+# ./release/etc
+# ./release/usr
+$MKFSJFFS2 -qUfv -p0x4000000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin
+$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.bin
+
+# Create a kathrein update file for fw's 
+# To get the partitions erased we first need to fake an yaffs2 update
+$MUP c $OUTFILE << EOF
+3
+0x00000000, 0x800000, 3, foo
+0x00800000, 0x4000000, 3, foo
+0x00400000, 0x0, 0, uImage
+0x00000000, 0x0, 1, mtd_fw.sum.bin
+0x00800000, 0x0, 1, mtd_root.sum.bin
+;
+EOF
+
+rm -f $CURDIR/uImage
+rm -f $CURDIR/mtd_fw.bin
+rm -f $CURDIR/mtd_root.bin
+rm -f $CURDIR/mtd_fw.sum.bin
+rm -f $CURDIR/mtd_root.sum.bin
+
+zip $OUTFILE.zip $OUTFILE
---
+++ tdt/flash/ufs913/ufs913.sh
@@ -0,0 +1,118 @@
+CURDIR=`pwd`
+BASEDIR=$CURDIR/../..
+
+
+TUFSBOXDIR=$BASEDIR/tufsbox
+CDKDIR=$BASEDIR/cvs/cdk
+
+SCRIPTDIR=$CURDIR/scripts
+TMPDIR=$CURDIR/tmp
+TMPROOTDIR=$TMPDIR/ROOT
+TMPKERNELDIR=$TMPDIR/KERNEL
+TMPFWDIR=$TMPDIR/FW
+
+OUTDIR=$CURDIR/out
+
+if [  -e $TMPDIR ]; then
+  rm -rf $TMPDIR/*
+fi
+
+mkdir $TMPDIR
+mkdir $TMPROOTDIR
+mkdir $TMPKERNELDIR
+mkdir $TMPFWDIR
+
+echo "This script creates flashable images for Kathrein UFS912"
+echo "Author: Schischu"
+echo "Date: 07-17-2012"
+echo "-----------------------------------------------------------------------"
+echo "It's expected that an image was already build prior to this execution!"
+echo "-----------------------------------------------------------------------"
+echo "Checking targets..."
+echo "Found targets:"
+if [  -e $TUFSBOXDIR/release ]; then
+  echo "   1) Prepare Enigma2"
+fi
+if [  -e $TUFSBOXDIR/release_neutrino ]; then
+  echo "   2) Prepare Neutrino"
+fi
+
+read -p "Select target (1-2)? "
+case "$REPLY" in
+	1)  echo "Preparing Enigma2 Root..."
+		$SCRIPTDIR/prepare_root.sh $CURDIR $TUFSBOXDIR/release $TMPROOTDIR $TMPKERNELDIR $TMPFWDIR;;
+	2)  echo "Preparing Neutrino Root..."
+		$SCRIPTDIR/prepare_root.sh $CURDIR $TUFSBOXDIR/release_neutrino $TMPROOTDIR $TMPKERNELDIR $TMPFWDIR;;
+	*)  "Invalid Input! Exiting..."
+		exit 2;;
+esac
+echo "Root prepared"
+echo "Checking if flashtool mup exists..."
+if [ ! -e $CURDIR/mup ]; then
+  echo "Flashtool mup is missing, trying to compile it..."
+  cd $CURDIR/../common/mup.src
+  $CURDIR/../common/mup.src/compile.sh
+  mv $CURDIR/../common/mup.src/mup $CURDIR/mup
+  cd $CURDIR
+  if [ ! -e $CURDIR/mup ]; then
+    echo "Compiling failed! Exiting..."
+    exit 3
+  else
+    echo "Compiling successfull"
+  fi
+fi
+echo "Flashtool mup exists"
+echo "-----------------------------------------------------------------------"
+echo "Checking targets..."
+echo "Found flashtarget:"
+echo "   1) KERNEL with ROOT"
+echo "   2) KERNEL with ROOT and FW"
+echo "   3) KERNEL"
+echo "   4) FW"
+read -p "Select flashtarget (1-4)? "
+case "$REPLY" in
+	1)  echo "Creating KERNEL with ROOT..."
+		$SCRIPTDIR/flash_part_wo_fw.sh $CURDIR $TUFSBOXDIR $OUTDIR $TMPKERNELDIR $TMPROOTDIR;;
+	2)  echo "Creating KERNEL with ROOT and FW..."
+		$SCRIPTDIR/flash_part_w_fw.sh $CURDIR $TUFSBOXDIR $OUTDIR $TMPKERNELDIR $TMPFWDIR $TMPROOTDIR;;
+	3)  echo "Creating KERNEL..."
+		$SCRIPTDIR/flash_part_kernel.sh $CURDIR $TUFSBOXDIR $OUTDIR $TMPKERNELDIR;;
+	4)  echo "Creating FW..."
+		$SCRIPTDIR/flash_part_fw.sh $CURDIR $TUFSBOXDIR $OUTDIR $TMPFWDIR;;
+	*)  "Invalid Input! Exiting..."
+		exit 3;;
+esac
+clear
+echo "-----------------------------------------------------------------------"
+AUDIOELFSIZE=`stat -c %s $TMPFWDIR/audio.elf`
+VIDEOELFSIZE=`stat -c %s $TMPFWDIR/video.elf`
+if [ $AUDIOELFSIZE == "0" ]; then
+  echo "!!! WARNING: AUDIOELF SIZE IS ZERO !!!"
+  echo "IF YOUR ARE CREATING THE FW PART MAKE SURE THAT YOU USE CORRECT ELFS"
+  echo "-----------------------------------------------------------------------"
+fi
+if [ $VIDEOELFSIZE == "0" ]; then
+  echo "!!! WARNING: VIDEOELF SIZE IS ZERO !!!"
+  echo "IF YOUR ARE CREATING THE FW PART MAKE SURE THAT YOU USE CORRECT ELFS"
+  echo "-----------------------------------------------------------------------"
+fi
+if [ ! -e $TMPROOTDIR/dev/mtd0 ]; then
+  echo "!!! WARNING: DEVS ARE MISSING !!!"
+  echo "IF YOUR ARE CREATING THE ROOT PART MAKE SURE THAT YOU USE A CORRECT DEV.TAR"
+  echo "-----------------------------------------------------------------------"
+fi
+
+echo ""
+echo ""
+echo ""
+echo "-----------------------------------------------------------------------"
+echo "Flashimage created:"
+echo `ls $OUTDIR`
+
+echo "-----------------------------------------------------------------------"
+echo "To flash the created image copy the *.img file to"
+echo "your usb drive in the subfolder /kathrein/ufs913/"
+echo ""
+echo "Remember, if this is the first time you flash Enigma2 or Neutrino,"
+echo "you will need to flash the updatescript.sh first."
+echo "The script can be found in the extras folder, copy it to /kathrein/ufs913/ on your usb drive."
