84b886073ee027e6cae6248e3bf5f7db548b7181
BPanther <bpanther_ts@hotmail.com>
2012-07-31 18:40:42 +0200

    stm24_p0211 - not yet ready

--- tdt/cvs/cdk/root/release/neutrino_version.sh
+++ tdt/cvs/cdk/root/release/neutrino_version.sh
@@ -1,48 +1,33 @@
 #!/bin/sh
+
+#################
+RELEASE=0
+BETA=1
+INTERNAL=2
+SNAPSHOT=$RELEASE
+TIMESTAMP=`date +%Y%m%d%H%M`
+#BUILDREV=`git describe | cut -f2 -d'-'`
+BUILDREV="100"
+#################
+
+CURDIR=`pwd`
+KATIDIR=${CURDIR%/cvs/cdk}
+version=`git describe`
+cat $KATIDIR/cvs/cdk/root/var/etc/.version | head -n 6 > $KATIDIR/cvs/cdk/root/var/etc/.version.new
+
 #########################################
 # Original Neutrino Release Cycle Format:
 # SBBBYYYYMMTTHHMM
-# | |   | | |  | |_MM	= Minutes 0 - 59
-# | |   | | |  |___HH	= Hours 0 - 23
-# | |   | | |______TT	= Day 0 - 31
-# | |   | |________MM	= Month 0 - 12
-# | |   |__________YYYY	= Year
-# | |______________BBB	= Build-Revision (interesting for Image-Groups!)
-# |________________S	= Snapshot Version: 0 = Release, 1 = Beta, 2 = Internal, L = Locale,
-#					    S = Settings, A = Addon, T = Text
+# | |   | | |  | |_MM=Minutes 0 -59
+# | |   | | |  |___HH=Hours 0 - 23
+# | |   | | |______TT=Day 0-31
+# | |   | |________MM=Month 0-12
+# | |   |__________YYYY=Year
+# | |______________BBB=Build-Revision (interesting for Image-Groups!)
+# |________________S=Snapshot Version: 0 = Release, 1 = Beta, 2 = Internal
 #########################################
-cd /tdt/tdt
-homepage="http://neutrino.mbremer.de"
-docs="http://forum.mbremer.de"
-forum="http://forum.mbremer.de"
-release="1"
-version="100"
-datum=`date +'%Y%m%d%H%M'`
-gitversion=`git describe | awk -F '-' '{print $1$2}'`
-stmversion=`cat /tdt/tdt/cvs/cdk/linux{,-sh4}/localversion-stm | awk -F '_' '{print $2"_"$3}'`
-plversion=`ls -l /tdt/tdt/cvs/driver/player2 | awk -F '_' '{print $2}'`
-echo "imagename=BPanther-Image ($stmversion, player$plversion)" > /tdt/tdt/cvs/cdk/root/var/etc/.version
-echo "homepage=$homepage" >> /tdt/tdt/cvs/cdk/root/var/etc/.version
-echo "creator=BPanther" >> /tdt/tdt/cvs/cdk/root/var/etc/.version
-echo "version=$release$version$datum" >> /tdt/tdt/cvs/cdk/root/var/etc/.version
-echo "git=$gitversion" >> /tdt/tdt/cvs/cdk/root/var/etc/.version
-echo "docs=$docs" >> /tdt/tdt/cvs/cdk/root/var/etc/.version
-echo "forum=$forum" >> /tdt/tdt/cvs/cdk/root/var/etc/.version
-cp /tdt/tdt/cvs/cdk/root/var/etc/.version /tdt/tdt/tufsbox/release_neutrino/var/etc/.version
-mv /tdt/tdt/tufsbox/release_neutrino/var/etc/.version /tdt/tdt/tufsbox/release_neutrino/.version
-ln -sf /.version /tdt/tdt/tufsbox/release_neutrino/var/etc/.version
-#########################################
-#stmver=`echo "$stmversion" | awk -F '_' '{print $1}'`
-#boxname=`cat /tdt/tdt/tufsbox/release_neutrino/etc/hostname`
-#if [ -e /tdt/tdt/cvs/cdk/own_build/neutrino/etc/.usbimage ]; then
-#	echo "http://$boxname.mbremer.de/USB-BA/$stmver/Neutrino-BA.list" > /tdt/tdt/tufsbox/release_neutrino/etc/update.urls
-#fi
-#if [ -e /tdt/tdt/cvs/cdk/own_build/neutrino/var/etc/.usbimage ]; then
-#	echo "http://$boxname.mbremer.de/USB-BA/$stmver/Neutrino-BA.list" > /tdt/tdt/tufsbox/release_neutrino/var/etc/update.urls
-#fi
-#if [ -e /tdt/tdt/cvs/cdk/own_build/neutrino/etc/.flashimage ]; then
-#	echo "http://$boxname.mbremer.de/FLASH/$stmver/Neutrino.list" > /tdt/tdt/tufsbox/release_neutrino/etc/update.urls
-#fi
-#if [ -e /tdt/tdt/cvs/cdk/own_build/neutrino/var/etc/.flashimage ]; then
-#	echo "http://$boxname.mbremer.de/FLASH/$stmver/Neutrino.list" > /tdt/tdt/tufsbox/release_neutrino/var/etc/update.urls
-#fi
+
+echo "version=$SNAPSHOT$BUILDREV$TIMESTAMP" >> $KATIDIR/cvs/cdk/root/var/etc/.version.new
+echo "git=$version" >> $KATIDIR/cvs/cdk/root/var/etc/.version.new
+mv $KATIDIR/cvs/cdk/root/var/etc/.version.new $KATIDIR/cvs/cdk/root/var/etc/.version
+exit
