7a83478dc695e6fdd08cb0abc2d7a862d5796711
Schischu <schischu@duckbox>
2012-09-16 16:54:43 +0200

    [xbmc] change some initial settings

--- tdt/cvs/cdk/make/release_xbmc.mk
+++ tdt/cvs/cdk/make/release_xbmc.mk
@@ -471,7 +471,7 @@ endif
 	rm -rf $(prefix)/release/usr/share/xbmc/system/keymaps/*
 	cp -a $(targetprefix)/usr/share/xbmc/system/keymaps/appcommand.xml $(prefix)/release/usr/share/xbmc/system/keymaps/
 	cp -a $(targetprefix)/usr/share/xbmc/system/keymaps/keyboard.xml $(prefix)/release/usr/share/xbmc/system/keymaps/
-	sed -i "s/<home>FirstPage<\/home>/<!-- home>FirstPage<\/home -->/g" $(prefix)/release/usr/share/xbmc/system/keymaps/keyboard.xml
+	sed -i "s/<home>FirstPage<\/home>/<home>PreviousMenu<\/home>/g" $(prefix)/release/usr/share/xbmc/system/keymaps/keyboard.xml
 #	cp -a $(targetprefix)/usr/share/xbmc/system/keymaps/remote.xml $(prefix)/release/usr/share/xbmc/system/keymaps/
 	cp $(buildprefix)/root/release/keymap_xbmc.xml $(prefix)/release/usr/share/xbmc/system/keymaps/duckbox.xml
 
