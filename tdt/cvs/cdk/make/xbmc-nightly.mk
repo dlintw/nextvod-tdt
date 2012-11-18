90f2975341dce157a60dadaf667725ffa0369773
Schischu <schischu@duckbox>
2012-11-18 22:54:29 +0100

    [xbmc] Add Alpha 7

--- tdt/cvs/cdk/make/xbmc-nightly.mk
+++ tdt/cvs/cdk/make/xbmc-nightly.mk
@@ -15,13 +15,15 @@ $(DEPDIR)/xbmc-nightly.do_prepare:
 	echo "1) Sat, 14 Apr 2012 12:36 - 460e79416c5cb13010456794f36f89d49d25da75"; \
 	echo "2) Sun, 10 Jun 2012 13:53 - 327710767d2257dad27e3885effba1d49d4557f0"; \
 	echo "3) Fr,  31 Aug 2012 22:34 - Frodo_alpha5 - 12840c28d8fbfd71c26be798ff6b13828b05b168"; \
-	echo "4) current inactive... comming soon, here is the next stable (case 4 == DIFF=4)"; \
+	echo "4) Fr,  31 Oct 2012 22:34 - Frodo_alpha7 - e292b1147bd89a7e53742e3e5039b9a906a3b1d0"; \
+	echo "5) current inactive... comming soon, here is the next stable (case 5 == DIFF=5)"; \
 	read -p "Select: "; \
 	echo "Selection: " $$REPLY; \
-	[ "$$REPLY" == "0" ] && DIFF="2"; \
+	[ "$$REPLY" == "0" ] && DIFF="4"; \
 	[ "$$REPLY" == "1" ] && DIFF="1" && REVISION="460e79416c5cb13010456794f36f89d49d25da75"; \
 	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION="327710767d2257dad27e3885effba1d49d4557f0"; \
 	[ "$$REPLY" == "3" ] && DIFF="3" && REVISION="12840c28d8fbfd71c26be798ff6b13828b05b168"; \
+	[ "$$REPLY" == "4" ] && DIFF="4" && REVISION="e292b1147bd89a7e53742e3e5039b9a906a3b1d0"; \
 	echo "Revision: " $$REVISION; \
 	[ -d "$(archivedir)/xbmc.git" ] && \
 	(cd $(archivedir)/xbmc.git; git pull ; git checkout HEAD; cd "$(buildprefix)";); \
@@ -40,7 +42,7 @@ $(DEPDIR)/xbmc-nightly.do_prepare:
 #			PYTHON_LDFLAGS='-L$(targetprefix)/usr/include/python2.6 -lpython2.6' \
 #			PYTHON_VERSION='2.6' \
 #endable webserver else httpapihandler will fail
-$(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libstgles libass libmpeg2 libmad jpeg libsamplerate libogg libvorbis libmodplug curl libflac bzip2 tiff lzo libz fontconfig libfribidi freetype sqlite libpng libpcre libcdio jasper yajl libmicrohttpd tinyxml python gstreamer gst_plugins_dvbmediasink expat libnfs
+$(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libstgles libass libmpeg2 libmad jpeg libsamplerate libogg libvorbis libmodplug curl libflac bzip2 tiff lzo libz fontconfig libfribidi freetype sqlite libpng libpcre libcdio jasper yajl libmicrohttpd tinyxml python gstreamer gst_plugins_dvbmediasink expat libnfs taglib
 	cd $(appsdir)/xbmc-nightly && \
 		$(BUILDENV) \
 		./bootstrap && \
@@ -52,6 +54,8 @@ $(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libstgles lib
 			PYTHON_SITE_PKG=$(targetprefix)/usr/lib/python2.6/site-packages \
 			PYTHON_CPPFLAGS=-I$(targetprefix)/usr/include/python2.6 \
 			PY_PATH=$(targetprefix)/usr \
+			SWIG_EXE=none \
+			JRE_EXE=none \
 			--disable-gl \
 			--enable-glesv1 \
 			--disable-gles \
@@ -82,7 +86,8 @@ $(appsdir)/xbmc-nightly/config.status: bootstrap libboost directfb libstgles lib
 			--enable-gstplayer \
 			--enable-dvdplayer \
 			--disable-pulse \
-			--disable-alsa
+			--disable-alsa \
+			--disable-ssh
 
 $(DEPDIR)/xbmc-nightly.do_compile: $(appsdir)/xbmc-nightly/config.status
 	cd $(appsdir)/xbmc-nightly && \
