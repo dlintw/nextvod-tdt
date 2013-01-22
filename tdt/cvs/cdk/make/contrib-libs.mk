fa3f1ad53f0346d5725df9f9186abc44c4cbfaa7
BPanther <bpanther_ts@hotmail.com>
2013-01-22 23:19:43 +0100

    graphlcd - fontconfig not needed

--- tdt/cvs/cdk/make/contrib-libs.mk
+++ tdt/cvs/cdk/make/contrib-libs.mk
@@ -2047,7 +2047,7 @@ $(DEPDIR)/%libusb: $(DEPDIR)/libusb.do_compile
 #
 # graphlcd
 #
-$(DEPDIR)/graphlcd.do_prepare: bootstrap fontconfig libusb @DEPENDS_graphlcd@
+$(DEPDIR)/graphlcd.do_prepare: bootstrap libusb @DEPENDS_graphlcd@
 	@PREPARE_graphlcd@
 	touch $@
 
